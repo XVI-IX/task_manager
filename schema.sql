d1b951c3c4e871bd53280c219126e19fd5b46b48
XVI-IX <oladoja14@gmail.com>
2023-10-18 09:54:08 +0100

    Update: add schema.sql

---
+++ schema.sql
@@ -0,0 +1,28 @@
+-- Create users table
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(50) UNIQUE,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL UNIQUE, -- store securely hashed password
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+-- Create Categories table
+CREATE TABLE IF NOT EXISTS categories (
+  category_id SERIAL PRIMARY KEY,
+  category_name VARCHAR(50) NOT NULL,
+);
+
+-- Create Tasks table
+CREATE TABLE IF NOT EXISTS tasks (
+  task_id SERIAL PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  description TEXT,
+  due_date TIMESTAMPTZ,
+  priority INTEGER DEFAULT 1,
+  user_id INTEGER REFERENCES users(id),
+  category_id INTEGER REFERENCES categories(category_id),
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  updated_at TIMESTAMPTZ DEFAULT NOW(),
+);
