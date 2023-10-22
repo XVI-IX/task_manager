57a75f486612663724a9395a53d8be033769d011
XVI-IX <oladoja14@gmail.com>
2023-10-22 22:39:53 +0100

    Update: update tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -31,6 +31,39 @@ describe("App e2e", () => {
 
     configService = app.get<ConfigService>(ConfigService);
 
+    describe('Auth', () => {
+
+      describe('Register', () => {});
+
+      describe('Login', () => {});
+
+      describe('Logout', () => {});
+    });
+
+    describe('Task', () => {
+
+      describe('Get list of all tasks for user', () => {});
+
+      describe('Create Tasks', () => {});
+
+      describe('Get a specific task by ID', () => {});
+
+      describe('Get a list of all tasks with a specific due date', () => {});
+
+      describe('Update a specific task', () => {});
+
+      describe('Delete a specific task', () => {});
+
+      describe('Get tasks with specified priority', () => {});
+    });
+
+    describe('User', () => {
+
+      describe('Get User Profile', () => {});
+
+      describe('Get User Dashboard', () => {});
+    });
+
   });
 
   afterAll(() => {
